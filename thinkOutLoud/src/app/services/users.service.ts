eefe8394da131aaa547abfa3a2c7ea97314d0a53
Daniel <danielmathers97@gmail.com>
2024-08-23 19:10:00 +0200

    new feat: add cahcing of data retrieved from relevant http requests in users service.

--- thinkOutLoud/src/app/services/users.service.ts
+++ thinkOutLoud/src/app/services/users.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ILoginForm, iUser } from '../Interfaces/Users';
-import { Observable } from 'rxjs';
+import { Observable, of, shareReplay, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
+  userCache$?: Observable<iUser | null> | null | undefined;
   constructor(private http: HttpClient) {}
 
   registerUser(formData: ILoginForm) {
@@ -19,9 +20,26 @@ export class UsersService {
     return this.http.post('/user/login', formData, { withCredentials: true });
   }
 
-  getUserData() {
-    return this.http.get<iUser | null>('/users/getUserOnlogin', {
-      withCredentials: true,
-    });
+  invalidateUserCache() {
+    this.userCache$ = null;
+  }
+
+  refreshUserCache() {
+    this.invalidateUserCache();
+    return this.getLoggedInUserData();
+  }
+
+  getLoggedInUserData() {
+    if (!this.userCache$) {
+      this.userCache$ = this.http
+        .get<iUser | null>('/users/getUserOnlogin', {
+          withCredentials: true,
+        })
+        .pipe(
+          tap((userData) => console.log('UserData is ' + userData)),
+          shareReplay(1)
+        );
+    }
+    return this.userCache$;
   }
 }
