54066e678cf2929b7f35a487d1365cc23c468a86
Daniel <danielmathers97@gmail.com>
2024-09-02 16:46:34 +0200

    refactor of feed component in order to learn the declarative apprach in angular
    Refactored the logic to use the declartive appreach. My orevious solution wokred but i wanted to learn how to do it teh delcartive way to gain a deeper understanding.

--- thinkOutLoud/src/app/feed/feed.component.ts
+++ thinkOutLoud/src/app/feed/feed.component.ts
@@ -1,8 +1,18 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IFeedPosts } from '../../interfaces/postInterfaces';
 import { PostsService } from '../services/posts.service';
-import { catchError, tap, of, Subscription } from 'rxjs';
-import { NgFor, NgIf } from '@angular/common';
+import {
+  catchError,
+  tap,
+  of,
+  Subscription,
+  BehaviorSubject,
+  Observable,
+  switchMap,
+  scan,
+  EMPTY,
+} from 'rxjs';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { FeedpostComponent } from '../feedpost/feedpost.component';
 import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 import { FeedbackMessageComponent } from '../feedback-message/feedback-message.component';
@@ -16,50 +26,49 @@ import { FeedbackMessageComponent } from '../feedback-message/feedback-message.c
     FeedpostComponent,
     InfiniteScrollDirective,
     FeedbackMessageComponent,
+    AsyncPipe,
   ],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.scss',
 })
-export class FeedComponent implements OnInit, OnDestroy {
-  page = 1;
+export class FeedComponent implements OnInit {
   pageSize = 10;
-  posts: IFeedPosts[] | [] = [];
+
   isLoading = false;
   serverError = '';
   postsSubscription$!: Subscription;
+  page$ = new BehaviorSubject<number>(1);
+  feedPosts$!: Observable<IFeedPosts[] | []>;
 
   constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
-    this.loadFeedPosts();
+    this.feedPosts$ = this.page$.pipe(
+      tap(() => (this.isLoading = true)),
+      switchMap((pageNum) =>
+        this.postsService.getAllFriendsPosts(pageNum, this.pageSize).pipe(
+          catchError((err) => {
+            this.serverError = err;
+            this.isLoading = false;
+            return EMPTY;
+          })
+        )
+      ),
+      scan((acc: IFeedPosts[] | [], value: IFeedPosts[]) => {
+        if (this.page$.value === 1) {
+          return value;
+        }
+        return (acc = [...acc, ...value]);
+      }, []),
+      tap(() => (this.isLoading = false))
+    );
   }
 
-  loadFeedPosts() {
-    this.isLoading = true;
-    this.postsSubscription$ = this.postsService
-      .getAllFriendsPosts(this.page, this.pageSize)
-      .pipe(
-        tap((postArray) => {
-          this.posts = [...this.posts, ...postArray];
-          this.isLoading = false;
-        }),
-        catchError((err) => {
-          this.serverError = err.message;
-          this.isLoading = false;
-          return of([]);
-        })
-      )
-      .subscribe();
-    this.page += 1;
+  loadMorePosts() {
+    this.page$.next(this.page$.value + 1);
   }
 
-  onFeedRefresh() {
-    this.page = 1;
-    this.posts = [];
-    this.loadFeedPosts();
-  }
-
-  ngOnDestroy(): void {
-    this.postsSubscription$?.unsubscribe();
+  refreshFeed() {
+    this.page$.next(1);
   }
 }
