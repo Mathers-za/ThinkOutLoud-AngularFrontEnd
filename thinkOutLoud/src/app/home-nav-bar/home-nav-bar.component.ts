b5b7dc7ebb3fbecb45e9b58dcc03dcd314756ba3
Daniel <danielmathers97@gmail.com>
2024-09-05 12:08:21 +0200

    refactor: Make  homenavbar component/html reactive

--- thinkOutLoud/src/app/home-nav-bar/home-nav-bar.component.ts
+++ thinkOutLoud/src/app/home-nav-bar/home-nav-bar.component.ts
@@ -2,8 +2,10 @@ import { Component, Input } from '@angular/core';
 import { IUser } from '../Interfaces/Users';
 import { RouterLink } from '@angular/router';
 import { CreateEditPostComponent } from '../create-edit-post/create-edit-post.component';
-import { NgIf } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { CloseModalOnClickOutisdeDirective } from '../shared/custom directives/close-modal-on-click-outisde.directive';
+import { UsersService } from '../services/users.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home-nav-bar',
@@ -13,16 +15,17 @@ import { CloseModalOnClickOutisdeDirective } from '../shared/custom directives/c
     CreateEditPostComponent,
     NgIf,
     CloseModalOnClickOutisdeDirective,
+    AsyncPipe,
   ],
   templateUrl: './home-nav-bar.component.html',
   styleUrl: './home-nav-bar.component.scss',
 })
 export class HomeNavBarComponent {
-  @Input() userData!: null | IUser;
-  initails =
-    (this.userData?.firstName[0].toLocaleUpperCase() || 'D') +
-    (this.userData?.lastName[0].toLocaleUpperCase() || 'M');
+  user$: Observable<IUser | null> = this.usersService.user$;
+
   showCreateEditPost = false;
+
+  constructor(private usersService: UsersService) {}
 
   toggleShowCreateEditPost() {
     this.showCreateEditPost = !this.showCreateEditPost;
